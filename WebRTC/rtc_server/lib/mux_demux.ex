66730f44dbd5040e32aa24b18e06e2068d91be97
Joe Frangoudes <jfrangoudes@gmail.com>
2019-10-08 10:23:19 +0100

    constructing malformed stun message, but doesnt fail with badarg or fail to send

--- WebRTC/rtc_server/lib/mux_demux.ex
+++ WebRTC/rtc_server/lib/mux_demux.ex
@@ -20,17 +20,15 @@ defmodule RtcServer.MuxerDemuxer do
   @impl true
   def init([]) do
     {:ok, socket} = :gen_udp.open(9999, [{:active, true}, :binary])
-    message_type = <<0x01::size(2)>>
-    message_length = <<0x4C::size(2)>>
-    magic_cookie = <<0x00::size(4)>>
-    transaction_id = <<0x00::size(12)>>
 
-    <<message_type::binary, message_length::binary, magic_cookie::binary, transaction_id::binary>>
-    |> IO.inspect()
+    message_type = <<0x01::integer-size(16)>>
+    message_length = <<0x00::integer-size(16)>>
+    magic_cookie = <<0x2112A442::integer-size(32)>>
+    transaction_id = <<0x2112A4422112A4422112A400::integer-size(96)>>
 
     :gen_udp.send(
       socket,
-      {192, 168, 8, 110},
+      {192, 168, 43, 148},
       5000,
       <<message_type::binary, message_length::binary, magic_cookie::binary,
         transaction_id::binary>>
