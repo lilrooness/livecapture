1aaf350ecae4c99d676d2edc14adf8cc3a661416
Joe Frangoudes <jfrangoudes@gmail.com>
2019-10-10 13:08:04 +0100

    sending properly structured stun messages (not to portn 5000)

--- WebRTC/rtc_server/lib/mux_demux.ex
+++ WebRTC/rtc_server/lib/mux_demux.ex
@@ -21,25 +21,43 @@ defmodule RtcServer.MuxerDemuxer do
   def init([]) do
     {:ok, socket} = :gen_udp.open(9999, [{:active, true}, :binary])
 
+    # :gen_udp.send(
+    #   socket,
+    #   {10, 249, 65, 117},
+    #   9876,
+    #   generate_stun_payload(:binding)
+    # )
+
+    {:ok, %__MODULE__{multiplexed_socket: socket}}
+  end
+
+  def generate_stun_payload(:binding) do
     message_type = <<0x01::integer-size(16)>>
     message_length = <<0x00::integer-size(16)>>
     magic_cookie = <<0x2112A442::integer-size(32)>>
-    transaction_id = <<0x2112A4422112A4422112A400::integer-size(96)>>
+    transaction_id = <<0x4E5469735577704E4B4B7271::integer-size(96)>>
 
-    :gen_udp.send(
-      socket,
-      {192, 168, 43, 148},
-      5000,
-      <<message_type::binary, message_length::binary, magic_cookie::binary,
-        transaction_id::binary>>
-    )
+    <<message_type::binary, message_length::binary, magic_cookie::binary, transaction_id::binary>>
+  end
 
-    {:ok, %__MODULE__{multiplexed_socket: socket}}
+  def generate_stun_payload(_type) do
+    Logger.error("Does not support generating stun payloads of type #{_type}")
   end
 
   @impl true
   def handle_info(data, state) do
     IO.inspect(data, label: :info_data)
+
+    case data do
+      <<0x0001::integer-size(16), length::integer-size(16), 0x2112A442::integer-size(16),
+        transaction_id::integer-size(96), attrs::binary>> ->
+        Logger.info("STUN BINDING REQUEST: ATTRS: #{attrs}")
+
+      <<0x0101::integer-size(16), length::integer-size(16), 0x2112A442::integer-size(16),
+        transaction_id::integer-size(96), attrs::binary>> ->
+        Logger.info("STUN BINDING SUCCESS: ATTRS: #{attrs} ")
+    end
+
     {:noreply, state}
   end
 
